8c2482bb1a79825eb2d462c31a3a754eb2b0c233
Jorge Vega <j.vega@mafasa.org>
2025-05-20 13:20:53 -0700

    Pantalla principal para listar los proyectos

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_api_rest/screens/project_list_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Gestor de Proyectos',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
+      home: ProjectListScreen(),
+    );
   }
 }
---
+++ lib/screens/project_list_screen.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_api_rest/models/project.dart';
+import 'package:flutter_api_rest/services/project_service.dart';
+
+class ProjectListScreen extends StatefulWidget {
+  const ProjectListScreen({super.key});
+
+  @override
+  State<ProjectListScreen> createState() => _ProjectListScreenState();
+}
+
+class _ProjectListScreenState extends State<ProjectListScreen> {
+  final PorjectService _projectService = PorjectService();
+  late Future<List<Project>> _futureProjects;
+
+  @override
+  void initState() {
+    super.initState();
+    _refreshProjects();
+  }
+
+  void _refreshProjects() {
+    setState(() {
+      _futureProjects = _projectService.getProjects();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Proyectos'),
+      ),
+      body: FutureBuilder<List<Project>>(
+        future: _futureProjects,
+        builder: (context, snapshot){
+          if(snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator(),);
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'),);
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No hay proyectos disponibles'),);
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                final project = snapshot.data![index];
+                return ListTile(
+                  title: Text(project.name),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.edit), 
+                        onPressed: () async {
+
+                        },
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () async {
+                          _showDeleteDialog(project);
+                        },
+                      )
+                    ],
+                  ),
+                  onTap: () {
+
+                  },
+                );
+              },
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+
+  void _showDeleteDialog(Project project) {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: const Text('Eliminar Proyecto'),
+        content: Text('¿Estás seguro de que deseas eliminar "${project.name}"?'),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(ctx).pop(),
+            child: const Text('Cancelar'),
+          ),
+          TextButton(
+            onPressed: () async {
+              Navigator.of(ctx).pop();
+              try {
+                await _projectService.deleteProject(project.id!);
+                _refreshProjects();
+                if (mounted) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Proyecto eliminado con éxtito'),)
+                  );
+                }
+              } catch (e) {
+                if (mounted) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Error al eliminar: $e'))
+                  );
+                }
+              }
+            },
+            child: const Text('Eliminar'),
+          )
+        ]
+
+      )
+    );
+  }
+}
